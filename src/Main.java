2011a006811f24437c3f2b6fc23de8e0e08fddf7
Aansh patel <118075566+anshp-30304@users.noreply.github.com>
2025-03-04 11:31:06 +1000

    Welcome message to new one

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
         System.out.printf("Hello Ryan");
         System.out.printf("Hello Ryan");
 
+
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
